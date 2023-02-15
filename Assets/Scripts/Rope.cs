c70ef3a8d85e7d396902e9a51b6298340018592e
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2023-02-15 18:55:09 +0300

    refactr

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Rope : MonoBehaviour
 {
     [SerializeField] private Transform _attachmentPoint;
+
     public Vector3 AttachmentPoint => _attachmentPoint.transform.position;
     public Rigidbody2D Rigidbody { get; private set; }
 
